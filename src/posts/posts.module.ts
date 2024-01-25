2ea1a53fa2aaca2f2cd67edf1c8d0358308f371c
Dongju Yeon <dongju1497@gmail.com>
2024-01-25 19:08:34 +0900

    posts 모듈 생성

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostsModel } from './entities/posts.entity'; // PostsModel 엔티티를 import 합니다.
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PostsModel])], // 여기서 PostsModel을 사용합니다.
   controllers: [PostsController],
   providers: [PostsService],
 })
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PostsModel } from './entities/posts.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(
+    // Repository: Model을 기반으로 DB와 소통
+
+    @InjectRepository(PostsModel)
+    private readonly postsRepository: Repository<PostsModel>,
+  ) {}
+
+  async getAllPosts() {
+    this.postsRepository.find();
+  }
+
+  async getPostById(id: number) {
+    const post = await this.postsRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!post) {
+      throw new NotFoundException();
+    }
+
+    return post;
+  }
+
+  async createPost(author: string, title: string, content: string) {
+    // 1) create -> 저장할 객체를 생성한다.
+    // 2) save -> 객체를 저장한다. (create 메서드에서 생성한 객체로)
+
+    const post = this.postsRepository.create({
+      author,
+      title,
+      content,
+      likeCount: 0,
+      commentCount: 0,
+    });
+    const newPost = await this.postsRepository.save(post);
+
+    return newPost;
+  }
+}
